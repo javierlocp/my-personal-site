9ac383836e96d9d11aac210b191b6bfe5ec015b0
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-09-25 16:25:06 +0700

    remove Paragraph.com embed

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -24,12 +24,12 @@ const Blog = () => {
               className="-mx-4 flex flex-col gap-1.5 rounded-md px-4 py-3 text-base text-neutral-50 transition-all duration-250 hover:bg-stone-50/5 hover:text-neutral-50 active:bg-stone-50/5"
             >
               <div className="flex items-center justify-between gap-4 md:gap-8">
-                <h2 className="max-w-[320px] truncate md:max-w-[500px]">{post.title}</h2>
+                <h2 className="max-w-[320px] truncate text-lg md:max-w-[500px]">{post.title}</h2>
                 <div className="block flex-grow border-t border-gray-400/25" />
                 <span className="shrink-0 font-mono text-xs text-neutral-400">
                   {new Date(post.date).toLocaleDateString('en-GB', {
                     day: 'numeric',
-                    month: 'numeric',
+                    month: 'short',
                   })}
                 </span>
               </div>
@@ -38,7 +38,6 @@ const Blog = () => {
           </li>
         ))}
       </ul>
-      <iframe className="mt-12" src="https://paragraph.com/@vestigia/embed" width="100%" height="280" />
     </>
   );
 };
