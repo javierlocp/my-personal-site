a22fa00e8fbc3761b0b09fbd60df0ff87498bc25
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-10-31 12:02:34 +0700

    Change copy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <>
       <div className="intro mb-4 flex flex-col gap-4">
-        <h1 className="text-pretty">I'm a product designer with 14 years of experience crafting mobile and web products for start-ups and large orgs.</h1>
+        <h1 className="text-pretty">I'm a product designer with over a decade of experience crafting mobile and web products for startups and large organizations.</h1>
         <p className="text-pretty">
           I lead the product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, where I define core protocol experiences and drive design strategy across products. Previously, I
           built and led design teams, scaled cross-functional collaboration, and improved design systems at <ExternalLink href="https://agoda.com/">Agoda</ExternalLink>,{' '}
