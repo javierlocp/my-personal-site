d8ebcbc5bd1468425f69031fc764873dc905c4b0
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-11-06 10:27:08 +0700

    revise intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ const Home = () => {
   return (
     <>
       <div className="intro mb-4 flex flex-col gap-4">
-        <h1 className="text-balance">I'm a product design lead with over a decade of experience crafting mobile & web apps for startups and large organizations.</h1>
-        <p className="text-balance">
-          Currently leading product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, where I define core protocol experiences and drive design strategy across products.
-          Previously, I built and led design teams, scaled cross-functional collaboration, and improved design systems at <ExternalLink href="https://agoda.com/">Agoda</ExternalLink>,{' '}
-          <ExternalLink href="https://brankas.com/">Brankas</ExternalLink>, <ExternalLink href="https://says.com/">Says</ExternalLink> and Yield App.
+        <h1>I'm a product design lead with over a decade of experience crafting mobile & web products for founders, startups and global organizations.</h1>
+        <p>
+          Currently, I lead product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, a blockchain built for safer DeFi. Previously at{' '}
+          <ExternalLink href="https://agoda.com/">Agoda</ExternalLink>, <ExternalLink href="https://brankas.com/">Brankas</ExternalLink>, <ExternalLink href="https://says.com/">Says</ExternalLink> and
+          Yield App.
         </p>
       </div>
       <BookCall />
