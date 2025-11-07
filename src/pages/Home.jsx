defa06916006d2ec566b8f14121f399cd73ce4e6
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-11-07 22:15:49 +0700

    update current status

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
       <div className="intro mb-4 flex flex-col gap-4">
         <h1>I'm a product design lead with over a decade of experience crafting mobile & web products for founders, startups and global organizations.</h1>
         <p>
-          Currently, I lead product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, a blockchain built for safer DeFi. Previously at{' '}
-          <ExternalLink href="https://agoda.com/">Agoda</ExternalLink>, <ExternalLink href="https://brankas.com/">Brankas</ExternalLink>, <ExternalLink href="https://says.com/">Says</ExternalLink> and
-          Yield App.
+          Currently taking a short break while exploring ideas for my next venture. Most recently, I led product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, a blockchain
+          built for safer DeFi. Previously at <ExternalLink href="https://agoda.com/">Agoda</ExternalLink>, <ExternalLink href="https://brankas.com/">Brankas</ExternalLink>,{' '}
+          <ExternalLink href="https://says.com/">Says</ExternalLink> and Yield App.
         </p>
       </div>
       <BookCall />
